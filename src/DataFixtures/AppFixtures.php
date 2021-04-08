49428ff8a1606d9ab6c99768973c5991799611b2
AlixRomain <romain.alix89@gmail.com>
2021-04-08 16:52:09 +0200

    Create method update for insert category

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,30 +2,40 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Serializer\Serializer;
 
 class AppFixtures extends Fixture
 {
+    private $repoCat;
+
+    public function __construct(CategoryRepository $repoCat)
+    {
+        $this->repoCat = $repoCat;
+    }
+
     public function load(ObjectManager $manager)
     {
         /* Récupération du contenu du fichier .json */
         $contenu_fichier_json = file_get_contents(__DIR__.'/tricks.json');
         /* Les données sont récupérées sous forme de tableau (true) */
         $tr = json_decode($contenu_fichier_json, true);
+
         foreach ($tr["cat"] as $key){
+
             foreach ($key["types"] as $val){
-                dump($val["name"]);
+                $categorie = new Category();
+                $categorie->setName($val["name"]);
+                $categorie->setDescription($val["description"]);
+                $categorie->setCategoryParent($val["catParent"]);
+                $manager->persist($categorie);
             }
-
-        }
-        foreach ($tr["cat"][1]["types"] as $val){
-
-            dump($val["name"]);
         }
 
-        dd();
+
         $manager->flush();
     }
 }
