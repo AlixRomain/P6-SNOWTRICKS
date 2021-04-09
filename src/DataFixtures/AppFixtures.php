31df3e42c7eb256fefac1dc957e2ed3b01eb1c76
AlixRomain <romain.alix89@gmail.com>
2021-04-09 11:18:38 +0200

    create a datafixtures method for insert fake User

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -34,52 +34,61 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        /* Récupération du contenu du fichier .json */
-        $contenu_fichier_json = file_get_contents(__DIR__.'/tricks.json');
-        /* Les données sont récupérées sous forme de tableau (true) */
-        $tr = json_decode($contenu_fichier_json, true);
+        //FOR LOAD METHOD WITH DROP TABLE "symfony console doctrine:fixtures:load"
+        //FOR LOAD METHOD WITHOUT DROP TABLE "symfony console doctrine:fixtures:load --append"
+        //METHOD FOR INSERT CATEGORY
         /*
-        foreach ($tr["cat"] as $key){
-            foreach ($key["types"] as $val){
-                $categorie = new Category();
-                $categorie->setName($val["name"]);
-                $categorie->setDescription($val["description"]);
-                $categorie->setCategoryParent($val["catParent"]);
-                $manager->persist($categorie);
-            }
-        }
-        */
-        //Création de 25 adhérents
-            for($i = 0; $i<25; $i++){
-                $roleUser[]= USER::ROLE_USER;
+         $contenu_fichier_json = file_get_contents(__DIR__.'/tricks.json');
+         $tr = json_decode($contenu_fichier_json, true);
+
+         foreach ($tr["cat"] as $key){
+             foreach ($key["types"] as $val){
+                 $categorie = new Category();
+                 $categorie->setName($val["name"]);
+                 $categorie->setDescription($val["description"]);
+                 $categorie->setCategoryParent($val["catParent"]);
+                 $manager->persist($categorie);
+             }
+         }
+         */
+        //Création de 20 adhérents
+        $roleUser[]= USER::ROLE_USER;
+            for($i = 0; $i<20; $i++){
+                $avatar  = mt_rand(1,20);
                 $adherent = new User();
                 $adherent   ->setLname($this->faker->lastName)
                     ->setFname($this->faker->firstName($genre=mt_rand(0,1)))
                     ->setRoles($roleUser)
                     ->setRgpd(1)
-                    ->setAvatar()
+                    ->setAvatar('profil_'.$avatar.'jpg')
                     ->setDateCreate($this->faker->dateTimeInInterval( '-2 years', 'now'))
-                    ->setDevise($this->faker->title)
+                    ->setDevise('Mr'.$adherent->getFname().' pour vous servir !')
                     ->setSlug($this->slug->slugify(strtolower($adherent->getFname().$adherent->getLname())))
-                    ->setEmail(strtolower($adherent->getFname())."@gmail.com")
+                    ->setEmail($adherent->getFname().'-'.$adherent->getlname().'@snowtrick.com')
                     ->setPassword($this->passwordEncoder->encodePassword($adherent,$adherent->getFname()));
                 //La méthode permet d'attribuer une ckef/réference à chaque valeur/objet.
                 //// Une sorte de tableau d'association connu gere par faker
                 /// "adherentX" est associé à l'objet $adherentX
                 $this->addReference("adherent".$i, $adherent);
+
+
                 $manager->persist($adherent);
             }
 
-            $adherentAdmin = new User();
+           /* $adherentAdmin = new User();
             $roleAdmin[]= USER::ROLE_ADMIN;
             $adherentAdmin      ->setLname("Alix")
                 ->setFname("Romain")
                 ->setEmail("toto@toto.com")
                 ->setPassword($this->passwordEncoder->encodePassword($adherentAdmin,'toto'))
-                ->setRoles($roleAdmin);
+                ->setRgpd(1)
+                ->setRoles($roleAdmin)
+                ->setAvatar('profil_'.$avatar.'jpg')
+                ->setDateCreate($this->faker->dateTimeInInterval( '-2 years', 'now'))
+                ->setDevise('Née pour coder')
+                ->setSlug($this->slug->slugify(strtolower($adherent->getFname().$adherent->getLname())));
             $manager->persist($adherentAdmin);
-
-
+            */
         $manager->flush();
     }
 }
